37c3e574ad765e817fa5ec816877c05155aba056
salmaazakii <salmazaki700@gmail.com>
2021-02-28 13:34:55 +0200

    json-server change

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json');
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-// server.use(middlewares);
-// server.use(router);
+server.use(middlewares);
+server.use(router);
 
-// server.listen(port);
+server.listen(port);
 ///////////
 
-const express = require('express');
-const app = express();
-const data = require('./db.json');
-let port = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const data = require('./db.json');
+// let port = process.env.PORT || 3000;
 
 
-app.get('/',(req,res)=>{
-    res.send(data);
-})
+// app.get('/',(req,res)=>{
+//     res.send(data);
+// })
 
-app.listen(port,()=>{
-    console.log(`this example is listening on ${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`this example is listening on ${port}`)
+// })
